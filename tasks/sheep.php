96bd5317060dd362503e9c165747b80aef7d5f3a
Gints <gints.ozolins@inbox.lv>
2021-02-18 13:28:38 +0200

    Uzdevums "Sheep"

--- tasks/sheep.php
+++ tasks/sheep.php
@@ -1,30 +1,17 @@
 <?php
 $animals = ['sheep', 'sheep', 'sheep', 'wolf', 'sheep', 'wolf', 'sheep', 'sheep'];
-// happy,happy,OMG,hehe,OMG,hehe,OMG,happy
+//Expected output:   happy,happy,OMG,hehe,OMG,hehe,OMG,happy
 
 for ($i = 0; $i < count($animals); $i++) {
-    if ($animals[$i] === 'sheep') {
-        if ($i == 0) {
-            if ($animals[$i + 1] === 'sheep') {
-                echo 'happy';
-            } else {
-                echo 'OMG';
-            }
-        }
-        if ($i !== 0 && $i !== count($animals) - 1 && $animals[$i + 1] === 'sheep' && $animals[$i - 1] === 'sheep') {
-            echo 'happy';
-        } elseif ($i !== 0 && $i !== count($animals) - 1 && ($animals[$i + 1] === 'wolf' || $animals[$i - 1] === 'wolf')) {
-            echo 'OMG';
-        }
-        if ($i == count($animals) - 1) {
-            if ($animals[$i - 1] === 'sheep') {
-                echo 'happy';
-            } else {
-                echo 'OMG';
-            }
-        }
-    } else {
+    if ($animals[$i] === 'wolf') {
         echo 'hehe';
+    } else {
+        if (isset($animals[$i + 1]) && $animals[$i + 1] === 'wolf'
+            || isset($animals[$i - 1]) && $animals[$i - 1] === 'wolf') {
+            echo 'OMG';
+        } else {
+            echo 'happy';
+        }
     }
     if ($i !== count($animals) - 1) {
         echo ',';
