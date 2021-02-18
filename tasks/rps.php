c4f0e0e1b0364615897adfde0228b1fe7caf1a13
Gints <gints.ozolins@inbox.lv>
2021-02-18 20:08:47 +0200

    Uzdevums "Rock, paper, scissors"

--- tasks/rps.php
+++ tasks/rps.php
@@ -4,14 +4,14 @@ echo 'Enter rock(r), paper(p) or scissors(s):';
 do {
     $isInputValid = false;
     $readine = readline('>> ');
-    if (ctype_alpha($readine) && in_array($readine, $options)) {
-        $user = $readine;
+    if (ctype_alpha($readine) && in_array(strtolower($readine), $options)) {
+        $user = substr($readine, 0, 1);
         $isInputValid = true;
     }
 } while (!$isInputValid);
 
 $computer = $options[rand(0, 2)];
-echo "You: ".$options[array_search($user,$options)+3]."  Computer: ".$options[array_search($computer,$options)+3];
+echo "You: " . $options[array_search($user, $options) + 3] . "  Computer: " . $options[array_search($computer, $options) + 3];
 switch ([$user, $computer]) {
     case ['r', 's']:
     case ['p', 'r']:
