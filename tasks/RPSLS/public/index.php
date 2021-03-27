cd72359c16fc8dd405264d2e4d51b272c3d1c063
Gints <gints.ozolins@inbox.lv>
2021-03-27 10:02:40 +0200

    Mājas darbs uz 27. martu

--- tasks/RPSLS/public/index.php
+++ tasks/RPSLS/public/index.php
@@ -7,7 +7,7 @@ use RPSLS\App\Game;
 
 require 'header.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['element'])) {
     $game = new Game($_POST['element']);
     require 'result.php';
 }
